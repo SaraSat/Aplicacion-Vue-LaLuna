2ec66b89e4e3378b3b988b2b6f8aa24b58f374ab
Sara Álvarez Turrión <sara_sat93@hotmail.com>
2020-05-29 10:01:20 +0200

    mejorado el formulario inicio, formularios actividades, se ha borrado el componente formularioActividad

--- src/views/Actividades.vue
+++ src/views/Actividades.vue
@@ -2,61 +2,68 @@
 
   <!--Vista de las actividades programadas-->
   <v-container>
-      <h1>Actividades previstas</h1>
+    <h1>Actividades previstas</h1>
 
       <!--Tarjeta de actividad, se genera un for para recorrer el objeto que contenga las diferentes actividades programadas
       y asi mostrar los datos de cada una con la misma estructura
       Se importa el componente FormularioActividad para agregar o editar
       Se importa la constante variables de Formulario actividad para poder acceder a gettes y setters de los datos-->
     <v-content>
-        <v-layout>
-            <v-flex>
-                <v-btn icon><v-icon @click="add=false" color="green">mdi-plus</v-icon></v-btn>
-            </v-flex>
-        </v-layout>
+      <!--Dialog para poder insertar una nueva tarjeta actividad -->
+      <v-dialog v-model="dialog"  persistent max-width="600px">
+        <template v-slot:activator="{on}">
+          <v-btn class="d-flex flex-column ml-12" v-on="on"  small="">
+            <v-icon color ="green darken-2" x-large >mdi-pencil</v-icon>
+            <span>Insertar</span>
+          </v-btn>
+        </template>
+        <v-card dark>
+          <v-card-title>
+              <v-text-field label="Introduce el nombre de la actividad" v-model="nombre"></v-text-field>
+          </v-card-title>
+          <v-card-subtitle>
+              Fin de Semana previsto:
+              <v-text-field label="introduce el dia" v-model="fecha"></v-text-field>
+          </v-card-subtitle>
+          <v-card-text>
+              <v-text-field label="introduce una breve descripción" v-model="desc"></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+              <v-btn class="success" type="submit">Aceptar</v-btn>
+              <v-btn class="error" @click="cancelar">Cancelar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
-        <!--Tarjetas de actividad-->
-        <v-layout wrap>
-            <v-flex v-for="(item,index) in items" :key="index"  mb-1 v-if= add>
-                <v-card dark xs12 md6>
-                    <v-card-title><h4>{{item.nombre}}</h4></v-card-title>
-                    <v-card-subtitle><h6>Fin de semana previsto: {{item.fecha}}</h6></v-card-subtitle>
-                    <v-card-text>{{item.desc}}</v-card-text>
-                    <v-card-actions>
-                        <v-btn class="success" @click="edit=false" >Editar</v-btn>
-                        <v-btn class="error" >Eliminar</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-layout>
-
-        <!--Componente FormularioActividad que se mostrará si la variable add es true-->
-        <v-layout v-if="add">
-            <v-flex>
-                    <FormularioActividad>
-                    </FormularioActividad>
-
-            </v-flex>
-        </v-layout>
-
-        <!--Componente FormularioActividad que se mostrará si la variable edit es true-->
-        <v-layout v-if="!edit">
-            <v-flex>
-                 <FormularioActividad :item="item">
-                  </FormularioActividad>
-            </v-flex>
-
-        </v-layout>
+      <!--Tarjetas de actividad-->
+      <v-layout wrap>
+          <v-flex v-for="(item,index) in items" :key="index"  mb-1 v-if= add>
+              <v-card dark xs12 md6>
+                  <v-card-title><h4 v-if="!ed">{{item.nombre}}</h4>
+                  <h4 v-if="ed"><v-text-field v-model="item.nombre"></v-text-field></h4>
+                  </v-card-title>
+                  <v-card-subtitle><h6 v-if="!ed">Fin de semana previsto: {{item.fecha}}</h6>
+                  <h6 v-if="ed"><v-text-field label="Fin de semana previsto: " v-model="item.fecha"></v-text-field></h6>
+                  </v-card-subtitle>
+                  <v-card-text v-if="!ed">{{item.desc}}</v-card-text>
+                  <v-card-text v-if="ed"><v-text-field v-model="item.desc"></v-text-field></v-card-text>
+                  <v-card-actions>
+                      <v-btn class="success" @click="ed=true" v-if="!ed" >Editar</v-btn>
+                      <v-btn class="success" @click="edit" v-if="ed" >Editar</v-btn>
+                      <v-btn class="error" @click="eliminar" v-if="!ed">Eliminar</v-btn>
+                      <v-btn class="error" @click="ed=false" v-if="ed">Cancelar</v-btn>
+                  </v-card-actions>
+              </v-card>
+          </v-flex>
+      </v-layout>
     </v-content>
-    </v-container>
+  </v-container>
 </template>
 <script>
 
-import FormularioActividad from "../components/FormularioActividad.vue";
 export default {
   name: "Actividades",
   components: {
-    FormularioActividad
   },
   computed:{
     items() {
@@ -70,11 +77,19 @@ export default {
     return {
       add: true, //-.>method editar
       edit: true, //-->method agregar
-      item:[]
+      item:[], 
+      nombre:'',
+      fecha:'',
+      desc:'',
     };
   },
   methods: {
+    edit(){
 
+    },
+    eliminar() {
+
+    },
   }
 
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,21 +7,27 @@
       <v-content>
       <!--Cards Información próxima actividad
       Se genera un for para recorrer el objeto que contiene los datos de la actividad y así poder mostrarlos -->
-        <v-layout v-for="(item,index) in items " :key="index" wrap v-if="valid">
+        <v-layout v-for="(item,index) in items " :key="index" wrap>
 
-            <!--Tarjeta con información del comienzo de la actividad-->
+            <!--Tarjeta con información del comienzo de la actividad
+            Contiene la condicio ed, de tal manera que si se presiona el botón editar, los campos serán inputs-->
             <v-flex xs12>
                 <v-card dark height="100%" class="jumbotron">
-                   <v-btn @click="valid=false" class="info float-right mt-4 mr-4">Editar</v-btn>
+                   <v-btn @click="ed=true" class="info float-right mt-4 mr-4">Editar</v-btn>
                     <v-card-title>
-                      <h1>{{item.dia}}</h1>  
+                      <h1 v-if="!ed">{{item.dia}}</h1>  
+                      <h1 v-if="ed"><v-text-field  v-model="item.dia"></v-text-field></h1>
                       </v-card-title>
                     <v-card-subtitle>
-                      <h4>{{item.fecha}} a las  {{item.hora}} en {{item.lugar}}</h4>  
+                      <h4 v-if="!ed">{{item.fecha}} a las  {{item.hora}} en {{item.lugar}}</h4>  
+                      <h4 v-if="ed"><v-text-field  v-model="item.dia"></v-text-field>a las 
+                      <v-text-field  v-model="item.hora"></v-text-field> en <v-text-field  v-model="item.lugar"></v-text-field></h4>
                     </v-card-subtitle>
                     <v-card-text>
-                      <p>{{item.desc}}</p>
-                      <p>La actividad tendrá un coste de {{item.precio}}€</p>
+                      <p v-if="!ed">{{item.desc}}</p>
+                      <p v-if="ed"><v-text-field  v-model="item.desc"></v-text-field></p>
+                      <p v-if="!ed">La actividad tendrá un coste de {{item.precio}}€</p>
+                      <p v-if="ed">La actividad tendrá un coste de <v-text-field  v-model="item.precio"></v-text-field> €</p>
                     </v-card-text>
                     <v-card-actions>
                         <iframe class="d-block w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3032.1354486690466!2d-3.6343836846003565!3d40.53859657935119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422cf780c46225%3A0xc4af6e1770c0aefe!2sCasa%20de%20las%20Asociaciones!5e0!3m2!1ses!2ses!4v1586739419365!5m2!1ses!2ses"
@@ -37,65 +43,18 @@
                       <h1>Recogida</h1>
                     </v-card-title>
                     <v-card-subtitle>
-                      <h4>{{item.fecha}} a las  {{item.horaF}} en {{item.lugarF}}</h4>  
+                      <h4 v-if="!ed">{{item.fecha}} a las  {{item.horaF}} en {{item.lugarF}}</h4>  
+                      <h4 v-if="ed">{{item.fecha}} a las <v-text-field  v-model="item.horaF"></v-text-field> en <v-text-field  v-model="item.lugarF"></v-text-field></h4>
                     </v-card-subtitle>
                     <v-card-actions>
                         <iframe class="d-block w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3032.1354486690466!2d-3.6343836846003565!3d40.53859657935119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422cf780c46225%3A0xc4af6e1770c0aefe!2sCasa%20de%20las%20Asociaciones!5e0!3m2!1ses!2ses!4v1586739419365!5m2!1ses!2ses"
                         width="mr-auto" height="350" frameborder="0" style="border:3;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                     </v-card-actions>
+                    <v-btn  class="info float-right mt-4 mr-4" v-if="ed" @click="edit">Aceptar</v-btn>
                 </v-card>
             </v-flex>
         </v-layout>
         <!--Fin Card informacion-->
-
-        <!--Formulario para poder editar Card de informacion, mostrará cuando la variable valid sea false, al contrario que la 
-        tarjeta de informacion
-        El formulario tiene la misma estructura que la tarjeta de info-->
-        <v-layout v-if="!valid">
-          <v-flex>
-            <v-form @submit.prevent="edit">
-              <v-card dark height="100%">
-                <v-card-title>
-                  <h2>
-                    <v-text-field v-model="item.dia" label=" Introduce el día"></v-text-field>
-                  </h2>
-                </v-card-title>
-                <v-card-subtitle>                
-                  <h4>
-                    <v-text-field v-model="item.fecha" label="Introduce la fecha: dd/mm/aaaa"></v-text-field>
-                    <v-text-field v-model="item.hora" label="Introduce la hora de comienzo: 00:00 "></v-text-field>
-                    <v-text-field v-model="item.lugar" label="Introduce el lugar de comienzo"></v-text-field>
-                  </h4>
-                </v-card-subtitle>
-                <v-card-text>
-                <p>
-                  <v-text-field v-model="item.desc" label="Introduce una descripción de la actividad"></v-text-field>
-                </p>
-                <p>
-                  <v-text-field v-model="item.precio" label="Introduce el precio: X"></v-text-field>
-                </p>
-                </v-card-text>
-            </v-card>
-            <v-card dark height="100%">
-              <v-card-title>
-                <h2>
-                  Recogida
-                </h2>
-              </v-card-title>
-              <v-card-subtitle>                
-                <h4>
-                  <v-text-field v-model="item.horaF" label="Introduce la hora de comienzo: 00:00 "></v-text-field>
-                  <v-text-field v-model="item.lugarF" label="Introduce el lugar de comienzo"></v-text-field>
-                </h4>
-              </v-card-subtitle>
-              <v-card-actions>              
-                <v-btn type="submit" class="info float-right mt-4 mr-4">Editar</v-btn>
-              </v-card-actions>
-            </v-card>
-            </v-form>
-          </v-flex>
-        </v-layout>
-        <!--Fin del formulario-->
     </v-content>
 
     </v-container>
@@ -110,7 +69,7 @@ export default {
   },
   data() {
     return {
-      valid: true, //v-if --> method edit
+      ed: false, //v-if --> method edit
       item:[],
       id:1,
     };
